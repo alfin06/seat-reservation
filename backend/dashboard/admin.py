e56a4cf7a9c203a6a39491ae40ade211593ae4f8
Luke <luke@example.com>
2025-05-06 13:24:37 +0800

    Remove deleted files

--- backend/dashboard/admin.py
+++
@@ -1,7 +0,0 @@
-#Nick
-from django.contrib import admin
-from .models import ClassRoom, Seat, Reservation
-
-admin.site.register(ClassRoom)
-admin.site.register(Seat)
-admin.site.register(Reservation)
